e344c2dc65cb20ad62855fb5dd2510c201148a2f
Mahdi <mahdi.tt.2005@gmail.com>
2024-09-22 01:59:59 +0330

    creat database
    dffdd

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class post(models.Model):
+    # author
+    # img
     titel= models.CharField(max_length=120)
     content= models.TextField()
+    # tag
+    # categore
+    cuntent_view=models.IntegerField(default=0)
+    status= models.BooleanField(default= False)
+    publish_date= models.DateTimeField(null=True)
+    created_date= models.DateTimeField(auto_now_add=True,null=True)
+    update_date= models.DateTimeField(auto_now=True,null=True)
