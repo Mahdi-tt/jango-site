61377ae97909ba49d7fced3715ad04116c3487b1
Mahdi <mahdi.tt.2005@gmail.com>
2024-09-25 00:46:15 +0330

    data blog
    ficks data in blog home

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from blog.views import blog_home
 from blog.views import blog_single
-
+from blog.views import test
 
 app_name='blog'
 urlpatterns = [
     path('',blog_home,name='blog_home'),
-    path('single',blog_single,name='blog_single')
+    path('single',blog_single,name='blog_single'),
+    path('test',test,name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import post
 def blog_home(request):
-    return render(request,'blog/blog-home.html')
+    posts = post.objects.filter(status=1)
+    context={'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 def blog_single(request):
     return render(request,'blog/blog-single.html')
+def test(request):
+    posts=post.objects.filter(status=0)
+    context={'posts':posts}
+    return render(request,'test.html',context)
