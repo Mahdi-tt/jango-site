b8f801c26d4ac74764fd62851aa18ac55caeacb1
Mahdi <mahdi.tt.2005@gmail.com>
2024-10-23 15:30:09 +0330

    fix erorre
    fix errore migrate 0006 delete migrate 0006 ta 00010 and --fake migrate and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class categor(models.Model):
@@ -25,3 +26,6 @@ class post(models.Model):
 
     def __str__(self):
         return f"{self.titel} "
+    
+    def get_absolute_url(self):
+        return reverse("blog:blog_single", kwargs={'pid':self.id})
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,8 +41,17 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'django.contrib.humanize',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'robots',
+    "debug_toolbar",
 ]
+
+# sitest frameworks
 SITE_ID = 2
+
+#robots 
+ROBOTS_USE_SITEMAP = False
+ROBOTS_USE_HOST = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -133,3 +143,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
