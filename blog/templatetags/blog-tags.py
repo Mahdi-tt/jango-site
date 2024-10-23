e4d37fa9f084a161ed86f7af8b9887a0656b6b0f
Mahdi <mahdi.tt.2005@gmail.com>
2024-10-23 20:20:36 +0330

    bd
    bfbfdb

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,6 +1,7 @@
 from django import template
 from blog.models import post
 from blog.models import categor
+from django.utils import timezone
 
 register = template.Library()
 
@@ -11,11 +12,18 @@ def latespost(arg=2):
 
 @register.inclusion_tag('blog/blog-category.html')
 def postcategory():
-    posts=post.objects.filter(status=1)
-    categories=categor.objects.all()
-    cat_dict = {}
-    for name in categories:
-        cat_dict[name]=posts.filter(categore=name).count()
-    return {'categorys':cat_dict}
+    now = timezone.now()
+    posts = post.objects.filter(publish_date__lte=now, status=1)
+    categories = categor.objects.all() 
+    category_posts = {}
+    for category in categories:
+        category_posts[category.name] = posts.filter(categore=category)
+    return {'category_posts': category_posts}
+
+    # categories=categor.objects.all()
+    # cat_dict = {}
+    # for name in categories:
+    #     cat_dict[name]=posts.filter(categore=name).count()
+    # return {'categorys':cat_dict}
 
     
